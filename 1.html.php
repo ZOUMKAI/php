c097ef4bb0afa9e5b045976f607e930386445727
ZOUMKAI <B12090040@std.must.edu.tw>
2025-04-12 11:48:00 +0800

    php的儲存庫

---
+++ 1.html.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+    <title>我的第一個php程式</title>
+    </head>
+    <body>
+        <h1>我的第一個php程式</h1>
+        <h2>
+            <?php
+                echo"hello world";
+            ?>
+        </h2>
+    </body>
+</html>
+//這一程式會直接在網頁上顯示我的第一個php程式
---
+++ 2.login.php
@@ -0,0 +1,8 @@
+<?php
+
+echo $_POST["id"];
+echo "<br>";
+echo $_POST["pwd"];
+
+?>
+//以上的程式是根據之後的mydb來設定的密碼和帳號
