c7da965d4a3893f70688100c24b480e2ed324f94
ZOUMKAI <B12090040@std.must.edu.tw>
2025-06-03 14:52:03 +0800

    Delete php(2025 5 27) directory

--- php(2025 5 27)/12.logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-  session_start();
-  unset($_SESSION["id"]);
-  echo"登出成功";
-  echo "<meta http-equiv=REFRESH content='3,url=2.login.html>"
-?>
-//設定使用者的登出連結
--- php(2025 5 27)/9.reset_counter.php
+++
@@ -1,8 +0,0 @@
-<?php
-    session_start();
-    unset($_SESSION["counter"]);
-    echo "counter重置成功....";
-    echo "<meta http-equiv=REFRESH content='2; url=8.counter.php'>";
-
-?>
-//這段程式是清除 session 中的計數器資料顯示訊息後並在2 秒自動跳回主頁
